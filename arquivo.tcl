ff6e75a56945e6a02b0be3b30c9114b318105854
ana meira <analiviameira@gmail.com>
2026-03-20 20:39:18 -0300

    primeiro commit ticco

---
+++ arquivo.tcl
@@ -0,0 +1,8 @@
+puts "ola mundo - entre aspas";
+# ESTE EH UM COMENTARIO NO INICIO DE UMA LINHA
+puts {ola mundo - entre chaves}
+puts {exemplo de sintaxe de comentario invalida} # *erro* - nao ha ponto e virgula
+puts "esta eh a linha 1"; puts "esta eh a linha 2"
+puts "olá mundo; com um ponto e virgula entre aspas"
+# palavras nao precisam ser colocas entre aspas a menos que contenham espacos
+puts olaMundo
